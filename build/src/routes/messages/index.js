70c524a527607c6e3552563d482114855a5a833b
Renan D. Gava <renandellecrodegava7040@gmail.com>
2024-12-16 20:16:32 -0300

    Delete build directory

--- build/src/routes/messages/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.menssageRoute = void 0;
-const express_1 = require("express");
-const messageController_1 = require("../../controller/messageController");
-const menssageRoute = (0, express_1.Router)();
-exports.menssageRoute = menssageRoute;
-menssageRoute.get("/send/wahtsapp", new messageController_1.MenssageController().handle);
